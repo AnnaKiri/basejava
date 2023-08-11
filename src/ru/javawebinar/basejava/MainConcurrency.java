b773b5e9c39c082d7f2f26026061c79b099155f2
Anna Kirillova <a.kirillova1812@gmail.com>
2023-08-11 16:34:42 +0400

    deadlock

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -14,7 +14,6 @@ public class MainConcurrency {
             @Override
             public void run() {
                 System.out.println(getName() + ", " + getState());
-                throw new IllegalStateException();
             }
         };
         thread0.start();
@@ -57,6 +56,25 @@ public class MainConcurrency {
 
         System.out.println(counter);
 
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+
+        new Thread(() -> doDeadLock(lock1, lock2)).start();
+        new Thread(() -> doDeadLock(lock2, lock1)).start();
+    }
+
+    private static void doDeadLock(String lock1, String lock2) {
+        synchronized (lock1) {
+            System.out.println(lock1 + "was taken");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            synchronized (lock2) {
+                System.out.println(lock2 + "was taken");
+            }
+        }
     }
 
     private synchronized void inc() {
