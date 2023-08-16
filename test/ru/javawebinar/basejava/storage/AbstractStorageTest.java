873df1ccbc402467356018637fc7d77e18a5c304
Anna Kirillova <a.kirillova1812@gmail.com>
2023-08-16 13:05:34 +0400

    Init DB and properties

--- test/ru/javawebinar/basejava/storage/AbstractStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractStorageTest.java
@@ -3,6 +3,7 @@ package ru.javawebinar.basejava.storage;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import ru.javawebinar.basejava.Config;
 import ru.javawebinar.basejava.ResumeTestData;
 import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
@@ -14,7 +15,7 @@ import java.util.List;
 public class AbstractStorageTest {
 
     protected static final String PATH = "C:\\IntelliJ IDEA workspace\\basejava\\storage";
-    protected static final File STORAGE_DIR = new File(PATH);
+    protected static final File STORAGE_DIR = Config.get().getStorageDir();
     protected final Storage storage;
 
     private static final String UUID_1 = "uuid1";
