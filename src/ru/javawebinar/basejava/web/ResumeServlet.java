e760ea81197123133c3d98af7e91bec732019625
Anna Kirillova <a.kirillova1812@gmail.com>
2023-08-25 13:38:21 +0400

    jsp

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.basejava.web;
 
 import ru.javawebinar.basejava.Config;
-import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.Storage;
 
 import javax.servlet.ServletException;
@@ -9,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
     private Storage storage;
@@ -27,22 +25,24 @@ public class ResumeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        String name = request.getParameter("name");
+//        request.setCharacterEncoding("UTF-8");
+//        String name = request.getParameter("name");
 //        response.setHeader("Content-Type", "text/html; charset=UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + "!");
+//        response.setContentType("text/html; charset=UTF-8");
+//        response.setCharacterEncoding("UTF-8");
+//        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + "!");
+//
+//        List<Resume> list = storage.getAllSorted();
+//        response.getWriter().write("<table>\n" + "  <tr>\n");
+//        response.getWriter().write("<th>uuid</th>\n" + "<th>full_name</th>\n" + "</tr>\n");
+//        for (Resume resume : list) {
+//            response.getWriter().write("<tr>\n" + "<td>" + resume.getUuid() + "</td>\n");
+//            response.getWriter().write("<td>" + resume.getFullName() + "</td>\n" + "</tr>\n");
+//        }
+//        response.getWriter().write("</table>");
 
-        List<Resume> list = storage.getAllSorted();
-        response.getWriter().write("<table>\n" + "  <tr>\n");
-        response.getWriter().write("<th>uuid</th>\n" + "<th>full_name</th>\n" + "</tr>\n");
-        for (Resume resume : list) {
-            response.getWriter().write("<tr>\n" + "<td>" + resume.getUuid() + "</td>\n");
-            response.getWriter().write("<td>" + resume.getFullName() + "</td>\n" + "</tr>\n");
-        }
-        response.getWriter().write("</table>");
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
-
 }
 
