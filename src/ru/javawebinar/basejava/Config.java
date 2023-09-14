b1088e5564bcb652542bec8c7d7704417857c579
Anna Kirillova <a.kirillova1812@gmail.com>
2023-09-14 18:17:18 +0400

    change way to get properties

--- src/ru/javawebinar/basejava/Config.java
+++ src/ru/javawebinar/basejava/Config.java
@@ -4,13 +4,12 @@ import ru.javawebinar.basejava.storage.SqlStorage;
 import ru.javawebinar.basejava.storage.Storage;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("C:\\IntelliJ IDEA workspace\\basejava\\config\\resumes.properties");
+    private static final String PROPS_FILE_NAME = "resumes.properties";
     private static final Config INSTANCE = new Config();
     private final Properties props = new Properties();
     private final File storageDir;
@@ -21,12 +20,15 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream(PROPS_FILE_NAME)) {
+            if (is == null) {
+                throw new IllegalStateException("Unable to find " + PROPS_FILE_NAME);
+            }
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS_FILE_NAME);
         }
     }
 
